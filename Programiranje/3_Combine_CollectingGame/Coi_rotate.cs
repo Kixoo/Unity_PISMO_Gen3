28495f721b2c9ec56d3b64a0459df6815a5a89ba
Cwekii <dominik.cvetkovski@gmail.com>
2019-12-04 09:49:26 +0100

    CoinCollectable

---
+++ Programiranje/3_Combine_CollectingGame/Coi_rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coi_rotate : MonoBehaviour
+{
+    public float rotateSpeed = 1.5f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+    }
+}
---
+++ Programiranje/3_Combine_CollectingGame/Coin_Collector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Collector : MonoBehaviour
+{
+    // Broj koliko smo pokupili predmeta
+    public int skupljenih_Objekata;
+    public AudioSource zvuk;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject); //Uništimo objekt koji pokupimo
+        skupljenih_Objekata++; //Povećaju nam se bodovi za 1
+        zvuk.Play(); //Efekt da smo pokupili
+    }
+}
