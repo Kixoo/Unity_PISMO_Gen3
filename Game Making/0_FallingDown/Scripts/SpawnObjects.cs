12eb87fb85d8adcfbc58dc24275d9a5f845b2908
Cwekii <dominik.cvetkovski@gmail.com>
2019-12-06 13:57:25 +0100

    Falling down

---
+++ Game Making/0_FallingDown/Scripts/SpawnObjects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject good;
+    public GameObject bad;
+
+    public Transform spawnPosition;
+
+    public float timer = 1;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            int randStvori = Random.Range(0, 2);
+            if(randStvori == 0)
+            {
+                Instantiate(good, new Vector3(Random.Range(-6, 6), 12, 0), Quaternion.identity);
+                timer = 1;
+            }
+            if (randStvori == 1)
+            {
+                Instantiate(bad, new Vector3(Random.Range(-6, 6), 12, 0), Quaternion.identity);
+                timer = 1;
+            }
+        }
+    }
+}
