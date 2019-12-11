c264098789d9ea2ad225bbead28e885ebc32f6fd
Cwekii <dominik.cvetkovski@gmail.com>
2019-12-11 12:22:52 +0100

    FPS

---
+++ Game Making/1_FPS/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public float damage;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag != "Gun" || coll.gameObject.tag != "Bullet" || coll.gameObject.tag != "Player" ||coll.gameObject.tag != "MainCamera")
+        {
+            var reducehealth = coll.GetComponent<Health>();
+            //Skini health za damage
+            Destroy(this.gameObject);
+        }
+    }
+}
