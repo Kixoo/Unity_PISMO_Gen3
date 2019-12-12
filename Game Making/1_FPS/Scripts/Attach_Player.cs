8838032a39c3de988dab60db3af8a9f0bfb3f02e
Cwekii <dominik.cvetkovski@gmail.com>
2019-12-12 10:03:41 +0100

    Attach

---
+++ Game Making/1_FPS/Scripts/Attach_Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach_Player : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.parent = this.gameObject.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
