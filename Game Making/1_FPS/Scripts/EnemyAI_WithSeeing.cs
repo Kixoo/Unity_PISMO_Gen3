0a80c596f31cda9db648ce1aa7db7978a543eba8
Cwekii <dominik.cvetkovski@gmail.com>
2019-12-12 09:42:35 +0100

    DZ i FPS Update

---
+++ Game Making/1_FPS/Scripts/EnemyAI_WithSeeing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_WithSeeing : MonoBehaviour
+{
+    //Obavezno imajte sphere collider koji ima veci radius i trigger aktivan jer on uocava igraca
+    GameObject Player;
+    public float speed;
+    public float Damage;
+    bool uočenIgrač = false;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        if(uočenIgrač == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            uočenIgrač = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //var health = other.gameObject.GetComponent<Health>();
+            //health.health_Current -= Damage;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Game Making/1_FPS/Scripts/FallingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float timeForFall;
+    Rigidbody rb;
+    bool staosam = false;
+    public Material aktivan;
+    public float movingSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.isKinematic = true;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(movingSpeed, 0, 0);
+        if(staosam == true)
+        {
+            timeForFall -= Time.deltaTime;
+            if(timeForFall <= 0)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                staosam = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            staosam = true;
+            this.gameObject.GetComponent<MeshRenderer>().material = aktivan;
+        }
+        if(coll.gameObject.tag == "Static_Platform")
+        {
+            Debug.Log("da");
+            movingSpeed *= -1;
+            Debug.Log(movingSpeed);
+        }
+    }
+}
