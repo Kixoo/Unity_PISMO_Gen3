ceef399662cb041ccd23f038a71ecbb1c12ba35a
Cwekii <dominik.cvetkovski@gmail.com>
2020-01-22 12:16:13 +0100

    Survive Clicker New
    Resolution forcer,
    loading

---
+++ Game Making/6_SurviveClicker/StartInUnity_Clicker/Assets/Scripts/ForceResolution.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour
+{
+    private void Start()
+    {
+        //Skripta radi samo za build
+        //postavili smo rezoluciju na 1280x720 koja je fullscreen
+        Screen.SetResolution(1280, 720, true);  
+    }
+}
--- Game Making/6_SurviveClicker/StartInUnity_Clicker/Assets/Scripts/MainMenu.cs
+++ Game Making/6_SurviveClicker/StartInUnity_Clicker/Assets/Scripts/MainMenu.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     //public Scene gameScene;
 
+    public GameObject loadPopup;
+
     public IsGameLoaded gameLoaded;
+
+    private void Start()
+    {
+        loadPopup.SetActive(false);
+    }
 
     public void LoadExistingFile()
     {
         gameLoaded.GameLoaded = true;
-        SceneManager.LoadScene(1);
+
+        loadPopup.SetActive(true);
+
+        StartCoroutine(LoadSceneAsync());
+
+
         Debug.Log("pop: " + GameSaveData.highestPopulation);
         Debug.Log("dan: " + GameSaveData.highestDay);
         Debug.Log("rana: " + GameSaveData.highestFood);
@@ -24,12 +37,32 @@ public class MainMenu : MonoBehaviour
     public void NewGame()
     {
         //SceneManager.LoadScene(gameScene.name);
-        SceneManager.LoadScene(1);
+        //SceneManager.LoadScene(1);
 
+        loadPopup.SetActive(true);
+
+        StartCoroutine(LoadSceneAsync());
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        //Učitava scenu u pozadini sadašnje scene
+        //Koristimo za izradu loading screena
+
+        //DZ Urediti loading screen (postak, bar koji se puni, loading circle, slike da se mjenjaju itd.)
+        Text loadText = loadPopup.GetComponentInChildren<Text>();
+        loadText.color = new Color(loadText.color.r, loadText.color.g, loadText.color.b, Mathf.PingPong(Time.time, 1));
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
