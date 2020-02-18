25879c60567d072a8523858daac22e54ac5eccac
Cwekii <dominik.cvetkovski@gmail.com>
2020-02-18 14:03:18 +0100

    Highscore

---
+++ Programiranje/10_Highscore/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public int score;
+    int maxScore;
+    public Text scoreText;
+    public Text highscoreText;
+
+    private void Start()
+    {
+        maxScore = PlayerPrefs.GetInt("Highscore");
+        highscoreText.text = maxScore.ToString();
+    }
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+        if(score > maxScore)
+        {
+            maxScore = score;
+            PlayerPrefs.SetInt("Highscore", maxScore);
+            highscoreText.text = maxScore.ToString();
+        }
+    }
+}
